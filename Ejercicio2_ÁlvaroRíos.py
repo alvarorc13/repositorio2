82f2124c994b016d8fd5c9bdb821185f825dd130
alvarorc13 <146371440+alvarorc13@users.noreply.github.com>
2023-10-19 10:25:18 +0200

    Add files via upload

---
+++ Ejercicio2_ÁlvaroRíos.py
@@ -0,0 +1,28 @@
+numero_anterior=int(input("Introduce un número: "))
+
+numero_guardado=0
+
+while numero_anterior!=0:
+    numero_guardado=numero_anterior
+numero=int(input("Introduce un nuevo numero: "))
+
+
+
+
+
+
+
+
+
+
+numero_posterior=int(input("Introduce un número: "))
+numero_guardado2=0
+
+while numero_posterior!=0:
+    numero_guardado2=numero_posterior
+numero_posterior=int(input("Introduce un número: "))
+
+
+
+
+
